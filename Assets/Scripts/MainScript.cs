2a2a8851d7ce2e0179f81e48c286748a19368e2a
quntnim <genshin_imp@dgsw.hs.kr>
2022-07-19 11:36:17 +0900

    Scripts folder added

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    public string SceneToLoad;
+    
+    public void LoadGame()
+    {
+        
+    }
+}
