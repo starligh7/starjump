b51008813e2721d09377d6665143b464ebfc7592
changwook987 <changwook987@naver.com>
2022-07-19 19:54:11 +0900

    player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,23 @@ public class Player : MonoBehaviour
     public float jumpPower = 1.0f;
     public float gravity = 9.8f;
 
+    public bool isGrounded;
+
     public Vector3 velocity;
+
+    public GameObject head;
 
     private float rotY = 0.0f; // rotation around the up/y axis
     private float rotX = 0.0f; // rotation around the right/x axis
 
     private CharacterController controller;
+    private Animator anim;
 
     private void Start()
     {
         cameraInit();
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,6 +36,7 @@ public class Player : MonoBehaviour
     private void cameraInit()
     {
         Vector3 rot = transform.localRotation.eulerAngles;
+
         rotY = rot.y;
         rotX = rot.x;
 
@@ -41,33 +48,63 @@ public class Player : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = -Input.GetAxis("Mouse Y");
 
+
         rotY += mouseX * mouseSensitivity * Time.deltaTime;
         rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
+
         rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
 
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        Debug.Log(new Vector2(rotX, rotY));
+        
+        Quaternion localRotation = Quaternion.Euler(0, rotY, 0);
         transform.rotation = localRotation;
+
+        Vector3 headRot = head.transform.localRotation.eulerAngles;
+        headRot.x = rotX;
+
+        head.transform.localRotation = Quaternion.Euler(headRot);
     }
 
     private Vector3 moveDir = Vector3.zero;
     private void move()
     {
+        isGrounded = controller.isGrounded;
 
-        if (controller.isGrounded)
+        if (isGrounded && velocity.y < 0)
         {
-            moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-            moveDir = Quaternion.Euler(0, transform.rotation.y, 0) * moveDir;
-
-            moveDir = transform.TransformDirection(moveDir);
-            moveDir *= moveSpeed;
-
-            if (Input.GetButton("Jump"))
-                moveDir.y = jumpPower;
+            velocity.y = 0f;
         }
 
-        moveDir.y -= Time.deltaTime * gravity;
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        controller.Move(moveDir * Time.deltaTime);
+        anim.SetFloat("x", x);
+        anim.SetFloat("y", z);
+
+
+        Vector3 move = transform.TransformDirection(x, 0, z);
+        move.y = 0;
+
+        anim.SetBool("isWalk", !(x == 0 && z == 0));
+
+        move.Normalize();
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (isGrounded)
+        {
+            // Changes the height position of the player..
+            if (Input.GetButton("Jump"))
+            {
+                velocity.y += Mathf.Sqrt(jumpPower * 3.0f * gravity);
+            }
+        }
+
+        velocity.x *= 0.5f;
+        velocity.z *= 0.5f;
+
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
